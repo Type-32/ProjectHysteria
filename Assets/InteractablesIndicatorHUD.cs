379f92b4090b3434f97767d5bca5b4aab2151fd1
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-07-31 15:08:26 -0400

    Commit

--- Assets/InteractablesIndicatorHUD.cs
+++ Assets/InteractablesIndicatorHUD.cs
@@ -1,3 +1,10 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Hysteria.Interface;
+using UnityEngine;
+using UnityEngine.UI;
+
 using System.Collections;
 using System.Collections.Generic;
 using Hysteria.Interface;
@@ -7,38 +14,63 @@ using UnityEngine.UI;
 public class InteractablesIndicatorHUD : MonoBehaviour
 {
     private Camera _camera;
-    private RectTransform trackerHolder;
+    [SerializeField] private RectTransform trackerHolder;
     private Dictionary<IInteractableObject, RectTransform> trackers = new();
     [SerializeField] private GameObject trackerPrefab;
+
     void Start()
     {
-        _camera = Camera.current;
+        _camera = Camera.main;
+        trackerHolder = GetComponent<RectTransform>();
     }
 
     void FixedUpdate()
     {
-        foreach (var obj in trackers.Keys)
+        UpdateTrackers();
+    }
+
+    private void UpdateTrackers()
+    {
+        foreach (var obj in new List<IInteractableObject>(trackers.Keys))
         {
-            trackers.TryGetValue(obj, out RectTransform value);
-            if (value)
-                value.position = _camera.WorldToScreenPoint(obj.GetGameObject().transform.position);
-            else
-                trackers.Remove(obj);
+            if (trackers.TryGetValue(obj, out RectTransform tracker))
+            {
+                if (tracker != null && obj.GetGameObject() != null)
+                {
+                    tracker.position = _camera.WorldToScreenPoint(obj.GetGameObject().transform.position);
+                }
+                else
+                {
+                    RemoveTrackingObject(obj);
+                }
+            }
         }
     }
     
     public RectTransform AddTrackingObject(IInteractableObject target, Color color, float ratioSize = 1f)
     {
+        if (trackers.ContainsKey(target))
+            return trackers[target];
+
         RectTransform obj = Instantiate(trackerPrefab, trackerHolder).GetComponent<RectTransform>();
         trackers.Add(target, obj);
 
         Image img = obj.GetComponentInChildren<Image>();
-        if (img.gameObject)
+        if (img != null)
         {
             img.color = color;
-            obj.localScale = new Vector3(obj.localScale.x * ratioSize, obj.localScale.y * ratioSize,
-                obj.localScale.z * ratioSize);
+            obj.localScale = new Vector3(obj.localScale.x * ratioSize, obj.localScale.y * ratioSize, obj.localScale.z * ratioSize);
         }
         return obj;
+    }
+
+    public void RemoveTrackingObject(IInteractableObject target)
+    {
+        if (trackers.TryGetValue(target, out RectTransform tracker))
+        {
+            if (tracker != null)
+                Destroy(tracker.gameObject);
+            trackers.Remove(target);
+        }
     }
 }
