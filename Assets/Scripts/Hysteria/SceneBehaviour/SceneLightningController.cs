28733a71347f0c9c6cae29195ee1d592f1e1ebb8
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-08-01 19:15:18 -0400

    h

---
+++ Assets/Scripts/Hysteria/SceneBehaviour/SceneLightningController.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Sirenix.OdinInspector;
+using UnityEngine;
+using UnityEngine.Experimental.GlobalIllumination;
+using Random = System.Random;
+
+namespace Hysteria.SceneBehaviour
+{
+    public class SceneLightningController : MonoBehaviour
+    {
+        [InfoBox("Put your directional light script here.")]
+        [SerializeField] Light directionalLight;
+
+        [MinMaxSlider(1, 4), SerializeField] private Vector2 flashCountRange;
+        [MinMaxSlider(10, 60), SerializeField] private Vector2 flashDurationIntervalSeconds;
+        [MinMaxSlider(0.01f, 0.1f), SerializeField] private Vector2 flashContinuousSecondsInterval;
+        [MinValue(0f), SerializeField] private float targetLightIntensity = 1000f;
+
+        private float _initialLightIntensity;
+        private bool _isFlashing = false;
+        private WaitForFixedUpdate _waitForFixedUpdate = new WaitForFixedUpdate();
+        private Random random;
+        private void Start()
+        {
+            random = new Random();
+            if (directionalLight == null)
+            {
+                Debug.LogError("Directional Light not assigned to SceneLightningController!");
+                enabled = false;
+                return;
+            }
+
+            _initialLightIntensity = directionalLight.intensity;
+            StartCoroutine(LightningRoutine());
+        }
+
+        private IEnumerator LightningRoutine()
+        {
+            while (true)
+            {
+                yield return new WaitForSeconds(random.Next((int)flashDurationIntervalSeconds.x, (int)flashDurationIntervalSeconds.y));
+
+                int flashCount = Mathf.RoundToInt(random.Next((int)flashCountRange.x, (int)flashCountRange.y));
+                for (int i = 0; i < flashCount; i++)
+                {
+                    StartCoroutine(FlashLightning());
+                    yield return new WaitForSeconds(random.Next((int)flashContinuousSecondsInterval.x * 100, (int)flashContinuousSecondsInterval.y * 100)/100f);
+                }
+            }
+        }
+
+        private IEnumerator FlashLightning()
+        {
+            _isFlashing = true;
+            directionalLight.intensity = targetLightIntensity;
+
+            yield return _waitForFixedUpdate;
+
+            directionalLight.intensity = _initialLightIntensity;
+            _isFlashing = false;
+        }
+        
+        private async Task FlashLightningTest(float duration = 0.1f)
+        {
+            _initialLightIntensity = directionalLight.intensity;
+
+            _isFlashing = true;
+            directionalLight.intensity = targetLightIntensity;
+
+            await Task.Delay((int)(duration * 1000));
+
+            directionalLight.intensity = _initialLightIntensity;
+            _isFlashing = false;
+        }
+
+        private void FixedUpdate()
+        {
+            if (directionalLight && !_isFlashing)
+            {
+                // Optionally, you can add some subtle random fluctuations here
+                directionalLight.intensity = _initialLightIntensity + random.Next(-5, 5);
+            }
+        }
+
+        [Button("Trigger Lightning")]
+        private async void TriggerLightning()
+        {
+            await FlashLightningTest();
+        }
+    }
+}
